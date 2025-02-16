171755ed17e51ee6026813243ae4f046cefc751c
Adamorris <adam.morrisroe@gmail.com>
2025-02-16 02:22:54 +0000

    Simple Calculator.py
    Added notes of what each line of code does

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,4 +1,4 @@
-a = input("First number: ")
-b = input("Second number: ")
-Sum = float(a) + float(b)
-print("Sum: " +str(Sum))
+a = input("First number: ") # 'a' = First number entered into calculator
+b = input("Second number: ") #'b' = Second number entered into calculator
+Sum = float(a) + float(b) # Sum = float(decimal number) of 'a' + float(decimal) number of 'b'
+print("Sum: " +str(Sum)) # Prints out the final calulation
