dca48018b6df5c0b8458a8aa48cbe402caa59534
Adamorris <adam.morrisroe@gmail.com>
2025-02-18 17:18:23 +0000

    Update Simple Weight Converter.py
    Made notes easier to read

--- Simple Weight Converter.py
+++ Simple Weight Converter.py
@@ -1,12 +1,12 @@
 
-weight = float(input("Weight: ")) # 'weight' equals the float version(integer with a decimal) of the integer/decimal the user types into the terminal
-unit = input("(K)g or (L)bs: ") # 'unit' equals either K or L depending on which the user inputs
-if unit.upper() == "K": # if the uppercase version of the input is K then ---
-    converted = weight / 0.45 # 'converted' equals the input of line 2 divided by 0.45 (this converts Kgs into Lbs)
-    print("Weight in Lbs: " + str(converted)) # Prints out the string version of the answer of line 5
-else: # if the uppercase version of the input of line 3 does not equal K ---
-    converted = weight * 0.45 # 'converted' equals the input of line 2 multiplied by 0.45 (this converts Lbs into Kgs
-    print("Weight in Kgs: " + str(converted)) # Prints the string version of the answer to line 8
+weight = float(input("Weight: "))             # 2: 'weight' equals the float version(integer with a decimal) of the integer/decimal the user types into the terminal
+unit = input("(K)g or (L)bs: ")               # 3: 'unit' equals either K or L depending on which the user inputs
+if unit.upper() == "K":                       # 4: if the uppercase version of the input is K then ---
+    converted = weight / 0.45                 # 5: 'converted' equals the input of line 2 divided by 0.45 (this converts Kgs into Lbs)
+    print("Weight in Lbs: " + str(converted)) # 6: Prints out the string version of the answer of line 5
+else:                                         # 7: if the uppercase version of the input of line 3 does not equal K ---
+    converted = weight * 0.45                 # 8: 'converted' equals the input of line 2 multiplied by 0.45 (this converts Lbs into Kgs
+    print("Weight in Kgs: " + str(converted)) # 9: Prints the string version of the answer to line 8
 # an integer is a whole number
 # a float is an integer with a decimal
 # a string is a piece of text surrounded by "" e.g "height"
