96bb6ca8eb1312e50985e3fb6930f5a640f3b641
Adamorris <adam.morrisroe@gmail.com>
2025-03-26 21:05:15 +0000

    Update Simple Calculator.py
    Changed the code so that now it will repeat until 2 numbers are given. Before it would just give an error message at the end of the code.

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,4 +1,14 @@
-a = input("First number: ") # 'a' = First number entered into calculator
-b = input("Second number: ") #'b' = Second number entered into calculator
-Sum = float(a) + float(b) # Sum = float(decimal number) of 'a' + float(decimal) number of 'b'
-print("Sum: " +str(Sum)) # Prints out the final calulation
+while True: # sets the following loop to function while true
+    try:
+         a = float(input("First number: ")) # a = number of user input
+         break # breaks the loop when a valid number is entered
+    except ValueError: # if the input is not a number then ---
+     print("Invalid - must be a number") # print invalid # Because the loop was never broken, it asks for the first number again. This will repeat until a valid number is entered
+while True: # sets the following loop to function while true 
+    try:
+         b = float(input("Second number: ")) # a = number of user input
+         break # breaks the loop when a valid number is entered
+    except ValueError: # if the input is not a number then ---
+     print("Invalid - must be a number") # print invalid # Because the loop was never broken, it asks for the first number again. This will repeat until a valid number is entered
+Sum = a + b # sets "sum" to equal "a" + "b"
+print("Sum: " +str(Sum)) # prints the word "sum:" and the sum created above as a string
