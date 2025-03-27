3f5703cccdbbfbe43b984e70dd7ff54e53706c47
Adamorris <adam.morrisroe@gmail.com>
2025-03-27 12:11:37 +0000

    Update Calculator.py
    Fixed a problem where the two numbers were always multiplied by each other.

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -4,7 +4,7 @@ while True:                                            # sets the following loop
          break                                         # breaks the loop when a valid number is entered
     except ValueError:                                 # if the input is not a number then ---
      print("Invalid - must be a number")               # print invalid # Because the loop was never broken, it asks for the first number again. This will repeat until a valid number is entered
-s = str(input("Enter symbol: "))                       # sets "s" to equal a symbol of the users choice e.g. *, +, -, /
+s = (input("Enter symbol: "))                       # sets "s" to equal a symbol of the users choice e.g. *, +, -, /
 while True:                                            # sets the following loop to function while true
     try:
          b = float(input("Second number: "))           # b = number of user input
@@ -12,11 +12,11 @@ while True:                                            # sets the following loop
     except ValueError:                                 # if the input is not a number then ---
      print("Invalid - must be a number")               # print invalid # Because the loop was never broken, it asks for the first number again. This will repeat until a valid number is entered
 if s == "+":                                           # if the user inputted + as s then ---
-    Sum = a + b                                        # sets "sum" to equal "a" + "b"
+    Sum = a + b                                       # sets "sum" to equal "a" + "b"
 if s == "-":                                           # if the user inputted - as s then ---
     Sum = a - b                                        # sets "sum" to equal "a" - "b"
-if s == "*" or "x" or "X":                             # if the user inputted * or x  as s then ---
+if s == "*":                                    # if the user inputted * or x  as s then ---
     Sum = a * b                                        # sets "sum" to equal "a" multiplied by "b"
 if s == "/":                                           # if the user inputted / as s then ---
     Sum = a / b                                        # sets "sum" to equal "a" divided by "b"
-print("Sum: " +str(Sum))                               # prints the word "sum:" and the sum created above as a string
+print("Sum: " + str(Sum))                              # prints the word "sum:" and the sum created above as a string
